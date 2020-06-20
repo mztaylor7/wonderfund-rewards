2867675c3c1a3982d986aac0ae09cd7e5030232f
zjayers <zayers.dev@gmail.com>
2020-06-20 11:58:53 -0700

    adds done() handlers to all synchronous test in server test suite

--- server/src/spec/server_spec/server.spec.js
+++ server/src/spec/server_spec/server.spec.js
@@ -15,45 +15,52 @@ describe('server', function () {
   let server;
   let database;
 
-  beforeEach(async function () {
+  beforeEach(function (done) {
     process.env.PORT = testPort;
     server = require('../../index');
     database = require('../../database');
+    done();
   });
 
   afterEach(async function () {
     await server.close();
   });
 
-  after(async function () {
+  after(function (done) {
     process.env = env;
+    done();
   });
 
   context('port', function () {
-    it('should be running on the specified port in process.env', function () {
+    it('should be running on the specified port in process.env', function (done) {
       const result = process.env.PORT;
       assert.equal(result, testPort);
+      done();
     });
 
-    it('should be running in the NODE_ENV "test" while in test mode', function () {
+    it('should be running in the NODE_ENV "test" while in test mode', function (done) {
       const result = process.env.NODE_ENV;
       assert.equal(result, 'test');
+      done();
     });
 
-    it(`should append '_test' to the end of the database name`, function () {
+    it(`should append '_test' to the end of the database name`, function (done) {
       const result = process.env.DATABASE_NAME;
       assert.isTrue(result.endsWith('_test'));
+      done();
     });
   });
 
   context('database success', function () {
-    it('should successfully connect to the database if the correct credentials are passed in', function () {
+    it('should successfully connect to the database if the correct credentials are passed in', function (done) {
       assert.isFulfilled(database.createSequelizeConnection());
+      done();
     });
 
-    it('should fail to connect to the database if incorrect credentials are passed in', function () {
+    it('should fail to connect to the database if incorrect credentials are passed in', function (done) {
       process.env.DATABASE_USER = '';
       assert.isRejected(database.createSequelizeConnection());
+      done();
     });
 
     it('should connect automatically to the database if not running in test mode', async function () {
