5e7f1f54e50962fa52e4ae7b25c4505f7ee905fe
zjayers <zayers.dev@gmail.com>
2020-06-19 19:08:05 -0700

    adds app component and prepares the client folder structure

---
+++ client/src/components/App/App.jsx
@@ -0,0 +1,14 @@
+/* Import Modules */
+import React from 'react';
+
+/**
+ * App Component
+ * @returns {*} JSX to be rendered to the page
+ * @constructor
+ */
+const App = () => {
+  return <div>App Works!</div>;
+};
+
+/* Export this module */
+export default App;
