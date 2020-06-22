6847794365d77c67a033305cac859412da95a206
zjayers <zayers.dev@gmail.com>
2020-06-22 00:19:48 -0700

    adds updates to seeding script to support console commands and chunking of generated data

--- server/src/dev-data/seeds/projectSeed.js
+++ server/src/dev-data/seeds/projectSeed.js
@@ -1,40 +1,69 @@
-/* eslint-disable no-plusplus,no-await-in-loop */
-// eslint-disable-next-line import/no-extraneous-dependencies
+/* eslint-disable import/no-extraneous-dependencies,global-require,no-await-in-loop */
 const path = require('path');
 const { config } = require('dotenv');
 
-config({ path: path.resolve(__dirname, '../../config/.env') });
+const inquirer = require('inquirer');
+const chalkPipe = require('chalk-pipe');
+const ProgressBar = require('progress');
+const _ = require('lodash');
+const PromiseBar = require('promise.bar');
+const chalk = require('chalk');
 
-/* APPEND _test to the end of the database name to avoid destroying our production database */
-/* Comment out this line to seed the production database */
-process.env.DATABASE_NAME += '_test';
-
-const database = require('../../database');
-
-const seedAmount = 100;
-
-const { generateMockProject, generateMockReward } = require('./generator');
+PromiseBar.enable();
 
 /**
  * Generate Projects Into Database
  */
-const generateProjects = async () => {
-  /* Create an array to hold promises for use with promise.all */
-  console.log('Seeding...');
+const generateProjects = async (dbName, seedAmount) => {
+  config({ path: path.resolve(__dirname, '../../config/.env') });
+
+  /* Set The Datbase Name */
+  process.env.DATABASE_NAME = dbName;
+
+  /* Require the datbase */
+  const database = require('../../database');
+
+  /* Require the generators for mock projects and rewards */
+  const { generateMockProject, generateMockReward } = require('./generator');
+
+  /* Connect to the database, drop all tables, then generate the projects in the database */
+  const connection = await database.createSequelizeConnection();
+  await connection.drop();
+
+  /* Create a progress bar to notify the user of staging percentage */
+  const stagingBar = new ProgressBar(
+    chalkPipe('green.bold')(
+      'Staged [:bar] :current of :total entries - :percent :etas'
+    ),
+    {
+      complete: '=',
+      incomplete: ' ',
+      width: 30,
+      total: seedAmount
+    }
+  );
+
   try {
+    /* Get the project and rward models */
     const Project = database.getProjectModel();
     const Reward = database.getRewardModel();
 
+    /* Sync the tables to ensure they exist */
     await Project.sync({ force: true });
     await Reward.sync({ force: true });
 
+    /* Max and min number of rewards to generate*/
     const maxRewards = 8;
     const minRewards = 3;
 
-    /* Loop from 1 to the seed amount and populate the database */
+    /* Create arrays to hold generated data */
+    const projects = [];
+    const rewards = [];
+
+    /* Loop from 1 to the seed amount and populate generator arrays */
     for (let i = 1; i <= seedAmount; i++) {
       const newProject = generateMockProject();
-      await Project.create(newProject);
+      projects.push(newProject);
 
       const randomNumber =
         Math.floor(Math.random() * (maxRewards - minRewards + 1)) + minRewards;
@@ -42,25 +71,103 @@ const generateProjects = async () => {
       for (let j = 1; j <= randomNumber; j++) {
         const newReward = generateMockReward();
         newReward.projectId = i;
-        await Reward.create(newReward);
+        rewards.push(newReward);
       }
+
+      /* Update the progress bar */
+      stagingBar.tick();
     }
-    console.log('Projects Table seeded successfuly!');
-    process.exit(0);
+
+    /* Split the generator arrays into chunks - this avoid packet_size errors for large seed operations */
+    const projectChunks = _.chunk(projects, seedAmount / (seedAmount * 0.01));
+    const rewardChunks = _.chunk(rewards, rewards.length / (seedAmount * 0.01));
+
+    /* Create a progress bar to notify the user of bulk creation progress */
+    const creationBar = new ProgressBar(
+      chalkPipe('green.bold')(
+        'Seeding [:bar] :current of :total chunks - :percent :etas'
+      ),
+      {
+        complete: '=',
+        incomplete: ' ',
+        width: 30,
+        total: projectChunks.length + rewardChunks.length
+      }
+    );
+
+    /* Array to hold bulk creations */
+    const creations = [];
+
+    /* Loop through the project chunks and bulk create each chunk */
+    projectChunks.forEach((chunk) => {
+      creations.push(Project.bulkCreate(chunk));
+      creationBar.tick();
+    });
+
+    /* Loop through the reward chunks and bulk create each chunk */
+    rewardChunks.forEach((chunk) => {
+      creations.push(Reward.bulkCreate(chunk));
+      creationBar.tick();
+    });
+
+    /* This is the same funciton as Promise.all - using the Promise.bar npm package to display the progress to the screen*/
+    PromiseBar.all(creations, {
+      label: chalk.blue('Verify'),
+      barFormat: chalk.dim.blue
+    }).then(() => {
+      /* All done! Notify user and exit! */
+      console.log(chalkPipe('green.bold')('Database Seeded Successfully!'));
+      process.exit(0);
+    });
   } catch (e) {
-    console.log(e);
+    /* Error! Notify user and exit! */
+    console.error(chalkPipe('red.bold')(e));
+    process.exit(0);
   }
 };
 
-/* Connect to the database, drop all tables, then generate the projects in the database */
-database
-  .createSequelizeConnection()
-  .then((connection) => {
-    return connection.drop();
-  })
-  .then(() => {
-    generateProjects();
-  })
-  .catch((err) => {
-    console.log(err);
+/**
+ * IIFE to run the console questionnaire upon module load
+ */
+(() => {
+  /* Mutable variables to hold the database name and seeding amount */
+  let dbName = 'kstart_test';
+  let seedAmount = 100;
+
+  /* Questions to present to the user in the console */
+  const questions = [
+    {
+      type: 'input',
+      name: 'database',
+      message: 'Enter the name of the database to seed',
+      default: dbName
+    },
+    {
+      type: 'number',
+      name: 'entries',
+      message: 'Enter the number of parent entries to create',
+      default: seedAmount
+    }
+  ];
+
+  /* Use the inquirer package to promt the user with questions and save the answers */
+  inquirer.prompt(questions).then((answers) => {
+    seedAmount = answers.entries;
+    dbName = answers.database;
+    return inquirer
+      .prompt([
+        {
+          type: 'confirm',
+          name: 'confirm',
+          message: chalkPipe('orange.bold')(
+            `Seed database: '${answers.database}' with ${answers.entries} entries?`
+          )
+        }
+      ])
+      .then((finalAnswer) => {
+        if (finalAnswer.confirm) {
+          return generateProjects(dbName, seedAmount);
+        }
+      });
   });
+})();
