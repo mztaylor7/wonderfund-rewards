b433ee8c7303ee64684487bdbe6781adad999868
zjayers <zayers.dev@gmail.com>
2020-06-29 14:27:39 -0700

    css fix for rewards viewer

--- client/src/components/RewardViewer/RewardViewer.style.js
+++ client/src/components/RewardViewer/RewardViewer.style.js
@@ -3,8 +3,6 @@ import styled from 'styled-components';
 export default styled.div`
   height: 100%;
   overflow: auto;
-  flex: 1;
-  position: fixed;
   padding-right: 20px;
   padding-bottom: 60px;
   padding-top: 40px;
