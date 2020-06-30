3955ce418008fb28c261d3a6b2a0d138346159b9
zjayers <zayers.dev@gmail.com>
2020-06-29 23:07:51 -0700

    updates axios pointers

--- client/src/services/apiService.js
+++ client/src/services/apiService.js
@@ -1,31 +1,26 @@
 import axios from 'axios';
-
+const serverAddr =
+  'http://ec2-18-216-10-147.us-east-2.compute.amazonaws.com:3005';
 const getParams = () => {
   const currentAddress = window.location.href;
   const n = currentAddress.lastIndexOf('/');
   return currentAddress.substring(n + 1);
 };
 
-const removeParams = () => {
-  const currentAddress = window.location.href;
-  const n = currentAddress.lastIndexOf('/');
-  return currentAddress.substring(0, n);
-};
-
 export const getRewards = () => {
-  return axios.get(`${removeParams()}/api/rewards`, {
+  return axios.get(`${serverAddr}/api/rewards`, {
     params: { projectId: getParams() },
   });
 };
 
 export const getUserInfo = () => {
-  return axios.get(`${removeParams()}}/api/projects/find`, {
+  return axios.get(`${serverAddr}}/api/projects/find`, {
     params: { id: getParams() },
   });
 };
 
 export const getUserImage = () => {
-  return axios.get(`${removeParams()}/api/projects/user`, {
+  return axios.get(`${serverAddr}/api/projects/user`, {
     params: { id: getParams() },
   });
 };
